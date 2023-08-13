04ef7093f899cf620db00943b863b32e63641254
galenomoon <galeno.dev@gmail.com>
2023-08-13 08:08:12 -0300

    coverage setup

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,11 @@ import type { Config } from "@jest/types";
 const config: Config.InitialOptions = {
   preset: "ts-jest", // interpret TypeScript files
   testEnvironment: "node", // run tests in Node.js environment
-  verbose: true, // output more information
+  verbose: true, // output more information,
+  collectCoverage: true, // collect coverage information
+  collectCoverageFrom: [
+    "<rootDir>/src/app/**/*.ts", // collect coverage from all .ts files in src folder
+  ],
 }
 
 export default config;
